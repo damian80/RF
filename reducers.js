4c7afcb486430a8c2055224d14355bd0cb9ad4ab
Damian Ciasnocha <daneek80@gmail.com>
2020-03-28 09:16:20 +0000

    Add files via upload

---
+++ reducers.js
@@ -0,0 +1,22 @@
+import {
+    CHANGE_SEARCH_FIELD,
+    REQUEST_ROBOTS_PENDING,
+    REQUEST_ROBOTS_SUCCESS,
+    REQUEST_ROBOTS_FAILED
+} from './constants.js'
+
+
+const intialState = {
+    searchField: ''
+}
+
+export const searchRobots = (state = intialState, action = {}) => {
+    switch (action.type) {
+        case CHANGE_SEARCH_FIELD:
+            return Object.assign({}, state, {
+                searchField: action.payload
+            });
+        default:
+            return state;
+    }
+}
